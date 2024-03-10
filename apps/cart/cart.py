dea7740ee1f77961d9a3e55ebea0883ba23c2bda
Volodya Sargsyan <volsarg@gmail.com>
2024-03-10 22:24:00 +0400

    Part 5

--- apps/cart/cart.py
+++ apps/cart/cart.py
@@ -28,7 +28,7 @@ class Cart(object):
             yield item
     
     def __len__(self):
-        return sum(item['quantity'] for item in self.cart.values())
+        return sum(int(item['quantity']) for item in self.cart.values())
 
     def add(self, product, quantity=1, update_quantity=False):
         product_id = str(product.id)
@@ -52,3 +52,7 @@ class Cart(object):
     def save(self):
         self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modifien = True
+
+
+    def get_total_length(self):
+        return sum(int(item['quantity']) for item in self.cart.values())
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -4,9 +4,20 @@ from .cart import Cart
 
 def cart_detail(request):
     cart = Cart(request)
+    productsstring = ''
+
+    for item in cart:
+        product = item['product']
+        b = "{'id': '%s', 'title': '%s', 'price': '%s', 'quantity': '%s', 'total_price': '%s'}," % (product.id, product.title, product.price, item['quantity'], item['total_price'])
+
+        productsstring = productsstring + b
+
+    print(cart.get_total_length(), 22222)
 
     context = {
-        'cart': cart
+        'cart': cart,
+        'productsstring': productsstring,
+        'prquantity': cart.get_total_length()
     }
 
     return render(request, 'cart.html', context)
